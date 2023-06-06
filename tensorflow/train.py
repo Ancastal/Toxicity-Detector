15481004f0a8ace24de9caaca5aaf7d8c06c2378
Antonio Castaldo <ancastal@outlook.it>
2023-06-06 15:27:46 +0200

    Moved files

--- tensorflow/train.py
+++ tensorflow/train.py
@@ -6,13 +6,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras import layers, Sequential
 import pickle
 
-
-
-dataset = load_dataset("mediabiasgroup/mbib-base", "hate-speech")
-df = pd.DataFrame(dataset['train'])
-
 def preprocess(df):
-
     X = df.text
     y = df.label
 
@@ -24,32 +18,41 @@ def preprocess(df):
     X_token = tk.texts_to_sequences(X)
 
     ### Pad the inputs
-    X_pad = pad_sequences(X_token, dtype='float32', padding='post')
+    X_pad = pad_sequences(X_token, dtype='float32', padding='post', truncating='post', maxlen=100)
 
     return X_pad, y, vocab_size
 
+
+dataset = load_dataset("mediabiasgroup/mbib-base", "hate-speech")
+df = pd.DataFrame(dataset['train'])
+
+
 X_pad, y, vocab_size = preprocess(df)
-### Let's build the neural network now
-# Size of your embedding space = size of the vector representing each word
 embedding_size = 100
 
-model = Sequential()
-model.add(layers.Embedding(
-    input_dim=vocab_size+1, # 16 +1 for the 0 padding
-    output_dim=embedding_size, # 100
-    mask_zero=True, # Built-in masking layer :)
-))
+def initialize_model():
+    model = Sequential()
+    model.add(layers.Embedding(
+        input_dim=vocab_size+1, # 16 +1 for the 0 padding
+        input_length=100,
+        output_dim=8, # 100
+        mask_zero=True, # Built-in masking layer :)
+    ))
+    model.add(layers.LSTM(20, return_sequences=True, activation="tanh"))
+    model.add(layers.Dense(1, activation="sigmoid"))
+    return model
 
-model.add(layers.LSTM(20))
-model.add(layers.Dense(1, activation="sigmoid"))
-model.summary()
+def compile_model(model):
+    model.compile(loss='binary_crossentropy',
+                optimizer='rmsprop',
+                metrics=['accuracy'],
+                )
+model = initialize_model()
+compile_model(model)
 
 es = EarlyStopping(patience=5, restore_best_weights=True)
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
-
-history = model.fit(X_pad, y, epochs=100, batch_size=64, verbose=1, validation_split=0.2, callbacks=[es])
+history = model.fit(X_pad, y, epochs=100, batch_size=64,
+                    verbose=1, validation_split=0.2, callbacks=[es])
 
 model.save('model.h5')
 model.save_weights('model_weights.h5')
