4deae1ab8b679954a52a0cb2550e10efbd6d6f98
Antonio Castaldo <ancastal@outlook.it>
2023-06-14 16:48:17 +0200

    changed

--- st-app.py
+++ st-app.py
@@ -1,30 +1,14 @@
 import streamlit as st
 import requests
-from google.cloud import storage
+import pandas as pd
 
-st.set_page_config(page_title="Toxicity Classifier", page_icon="🤖")
+st.set_page_config(page_title="Toxicity Detector", page_icon="🤖")
 
-model_list = [
-    "logistic_regression",
-    "decision_tree",
-    "random_forest",
-    "naive_bayes",
-    "svm",
-    "knn",
-    "gradient_boosting",
-    "xgboost",
-    "catboost",
-    "mlp",
-    "ada_boost",
-]
 
-st.title("🤖 Toxicity Classifier")
+st.title("🤖 Toxicity Detector")
 st.divider()
 st.image("https://nohatespeech.network/wp-content/uploads/2020/10/cropped-No-Hate-Speech-Logo-Transperent-1-1.png", width=600)
-st.write("Please select a model and enter a sentence for **toxicity classification**.")
-
-# Select a model
-model = st.selectbox("🔍 Select a model:", model_list)
+st.write("Please, enter a sentence for **toxicity classification**.")
 
 # Sentence input
 sentence = st.text_input("📝 Enter a sentence:")
@@ -35,11 +19,23 @@ button = st.button("Predict")
 if button:
     if sentence:
         st.write(f"🔄 Predicting...")
-        response = requests.get(f"https://toxicity-classifier-cwkm3of3qa-ew.a.run.app/predict?sentence={sentence}&model={model}")
+        response = requests.get(f"https://toxicity-classifier-cwkm3of3qa-ew.a.run.app/predict?sentence={sentence}")
         toxic = response.json()
+        pipe_fake = list(toxic[0].values())[0]
+        pipe_gender = list(toxic[1].values())[0]
+        pipe_hate = list(toxic[2].values())[0]
+        pipe_political = list(toxic[3].values())[0]
+        pipe_racial = list(toxic[4].values())[0]
 
-        # Display prediction
-        st.markdown(f"**🔮 Prediction:** `{'Toxic' if toxic['y_pred'] == 1 else 'Unbiased'}`")
-        st.markdown(f"**📊 Probabilities:** `{toxic['y_proba']}`")
+        print(pipe_fake)
+
+
+        st.markdown("### **🔮 Prediction:**")
+        st.write(f"**Hate Speech:** `{'Yes' if pipe_hate == 1 else 'No'}`")
+        st.write(f"**Political Bias:** `{'Yes' if pipe_political == 1 else 'No'}`")
+        st.write(f"**Gender Bias:** `{'Yes' if pipe_gender == 1 else 'No'}`")
+        st.write(f"**Racial Bias:** `{'Yes' if pipe_racial == 1 else 'No'}`")
+        st.write(f"**Fake News:** `{'Yes' if pipe_fake == 1 else 'No'}`")
+
     else:
         st.write("⚠️ Please enter a sentence.")
